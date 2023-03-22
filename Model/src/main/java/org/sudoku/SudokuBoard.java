f5676363d8ae7df4820a3596c872d5434123d62a
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2023-03-22 23:49:44 +0100

    Maven is working, checkstyle added and done. (It's not checking tests, idk if it's how should be)

--- Model/src/main/java/org/sudoku/SudokuBoard.java
+++ Model/src/main/java/org/sudoku/SudokuBoard.java
@@ -1,34 +1,33 @@
 package org.sudoku;
 
 import com.google.common.base.Objects;
-
 import java.util.Random;
 
 
 public class SudokuBoard {
 
-    private int[][] ClassBoard;
+    private int[][] classBoard;
     //I am using Backtracking algorithm to solve Sudoku.
     //TODO::REMEMBER, WE ARE USING NOT COPIED BOARD HERE
 
 
     public SudokuBoard() {
-        ClassBoard = new int[9][9];
-        fillBoard(ClassBoard);
+        classBoard = new int[9][9];
+        fillBoard(classBoard);
     }
 
-    private static boolean fillBoard (int [][] board){
+    private static boolean fillBoard(int [][] board) {
         Random random = new Random();
         int row = -1;
         int column = -1;
         boolean stillEmpty = true;
         int number = 0;
-        int[] numbers = new int[9];
+
 
         //I'm checking if board is still not fully filled.
 
-        for (int i = 0; i<9; i++){
-            for (int j = 0; j< 9; j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 if (board[i][j] == 0) {
                     row = i;
                     column = j;
@@ -36,17 +35,19 @@ public class SudokuBoard {
                     break;
                 }
             }
-            if(!stillEmpty)
+            if (!stillEmpty) {
                 break;
+            }
         }
-        if(stillEmpty)
+        if (stillEmpty) {
             return true;
+        }
 
-        for(int i = 1; i <15; i++){ //TODO::I don't like this for, think about sth better.
+        for (int i = 1; i < 15; i++) { //TODO::I don't like this for, think about sth better.
             number = random.nextInt(9) + 1;
-            if(isSafe(board, number, row, column)){
+            if (isSafe(board, number, row, column)) {
                 board[row][column] = number;
-                if(fillBoard(board)){
+                if (fillBoard(board)) {
                     return true;
                 } else {
                     board[row][column] = 0;
@@ -58,15 +59,15 @@ public class SudokuBoard {
     }
 
     //TODO::BOARD[ROW][COLUMN]
-    private static boolean isSafe(int[][] board, int numberToFill, int row, int column){
-        for(int i = 0; i< 9; i++){  //rows
-            if(board[row][i] == numberToFill){
+    private static boolean isSafe(int[][] board, int numberToFill, int row, int column) {
+        for (int i = 0; i < 9; i++) {  //rows
+            if (board[row][i] == numberToFill) {
                 return false;
             }
         }
 
-        for(int i = 0; i< 9; i++){  //columns
-            if(board[i][column] == numberToFill){
+        for (int i = 0; i < 9; i++) {  //columns
+            if (board[i][column] == numberToFill) {
                 return false;
             }
         }
@@ -76,8 +77,7 @@ public class SudokuBoard {
 
         for (int r = boxRowStart; r < boxRowStart + 3; r++) {
             for (int d = boxColStart; d < boxColStart + 3; d++) {
-                if (board[r][d] == numberToFill)
-                {
+                if (board[r][d] == numberToFill) {
                     return false;
                 }
             }
@@ -86,21 +86,22 @@ public class SudokuBoard {
         return true;
     }
 
-    public boolean checkBoard (){
-        for(int i = 0; i<9; i++){       //rows
-            for(int j = 0;j<9; j++){
-                for(int k = j+1; k<9; k++){
-                    if(ClassBoard[i][j] == ClassBoard[i][k]){
+    public boolean checkBoard() {
+        for (int i = 0; i < 9; i++) {       //rows
+            for (int j = 0; j < 9; j++) {
+                for (int k = j + 1; k < 9; k++) {
+                    if (classBoard[i][j] == classBoard[i][k]) {
                         return false;
                     }
                 }
             }
         }
 
-        for(int i = 0; i<9; i++){       //columns
-            for(int j = 0;j<9; j++){
-                for(int k = j+1; k<9; k++){
-                    if(ClassBoard[j][i] == ClassBoard[k][i]){
+
+        for (int i = 0; i < 9; i++) {       //columns
+            for (int j = 0; j < 9; j++) {
+                for (int k = j + 1; k < 9; k++) {
+                    if (classBoard[j][i] == classBoard[k][i]) {
                         return false;
                     }
                 }
@@ -111,16 +112,15 @@ public class SudokuBoard {
         int boxColStart;
         //TODO::My eyes are bleeding rn, but at least it works.
 
-        for(int row = 0; row< 9; row++){
-            for(int column = 0; column<9; column+=3){
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column += 3) {
                 boxRowStart = row - row % 3;
                 boxColStart = column - column % 3;
                 for (int r = boxRowStart; r < boxRowStart + 3; r++) {
                     for (int d = boxColStart; d < boxColStart + 3; d++) {
-                        for(int k = r+1; k<boxRowStart + 3; k++){
-                            for(int l = d+1; l <boxRowStart + 3; l++){
-                                if (ClassBoard[r][d] == ClassBoard[k][l])
-                                {
+                        for (int k = r + 1; k < boxRowStart + 3; k++) {
+                            for (int l = d + 1; l < boxRowStart + 3; l++) {
+                                if (classBoard[r][d] == classBoard[k][l]) {
                                     return false;
                                 }
                             }
@@ -136,19 +136,19 @@ public class SudokuBoard {
 
 
     public int getValue(int row, int column) {
-        return ClassBoard[row][column];
+        return classBoard[row][column];
     }
 
     public void setValue(int row, int column, int valueToSet) {
-        ClassBoard[row][column] = valueToSet;
+        classBoard[row][column] = valueToSet;
     }
 
     @Override
     public String toString() {
         StringBuilder msg = new StringBuilder();
-        for(int i = 0; i<9; i++){
-            for(int j = 0; j<9; j++){
-                msg.append(ClassBoard[i][j]).append("  ");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                msg.append(classBoard[i][j]).append("  ");
             }
             msg.append("\n");
         }
@@ -158,14 +158,18 @@ public class SudokuBoard {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         SudokuBoard that = (SudokuBoard) o;
-        return Objects.equal(ClassBoard, that.ClassBoard);
+        return Objects.equal(classBoard, that.classBoard);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode((Object) ClassBoard);
+        return Objects.hashCode((Object) classBoard);
     }
 }
