49c1989007214958f108b79d18efb6e42d9957c9
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2023-03-22 23:16:20 +0100

    Minor changes

--- Model/src/test/java/org/sudoku/SudokuBoardTest.java
+++ Model/src/test/java/org/sudoku/SudokuBoardTest.java
@@ -7,12 +7,16 @@ import org.junit.jupiter.api.Assertions;
 public class SudokuBoardTest {
 
     SudokuBoard board = new SudokuBoard();
+    SudokuBoard board1 = new SudokuBoard();
+    SudokuBoard board2 = new SudokuBoard();
 
 
     @Test
     void toStringTest() {
 
         System.out.println(board.toString());
+        System.out.println(board1.toString());
+        System.out.println(board2.toString());
     }
 
 }
